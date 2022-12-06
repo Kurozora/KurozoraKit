8d98b5542c6b34740ed600868e4b089f1b683b0b
kiritokatklian <casillaskhoren1@gmail.com>
2022-12-06 10:16:00 +0100

    [Add] Profile Update Request - Added ProfileUpdateRequest struct - Refactored updateInformation to return RequestSender instead of a closure - Updated updateInformation API to use ProfileUpdateRequest - Updated updateInformation's handling of profile and banner images

--- KurozoraKit/Classes/Extensions/KurozoraKit/KurozoraKit+Me.swift
+++ KurozoraKit/Classes/Extensions/KurozoraKit/KurozoraKit+Me.swift
@@ -42,67 +42,75 @@ extension KurozoraKit {
 	/// Updates the authenticated user's profile information.
 	///
 	/// Send `nil` if an infomration shouldn't be updated, otherwise send an empty instance to unset an information.
+	///
 	/// - Parameters:
 	///    - biography: The user's new biography.
 	///    - profileImage: The user's new profile image.
 	///    - bannerImage: The user's new profile image.
 	///    - username: The user's new username.
-	///    - completionHandler: A closure returning a value that represents either a success or a failure, including an associated value in each case.
-	///    - result: A value that represents either a success or a failure, including an associated value in each case.
-	public func updateInformation(biography: String? = nil, profileImage: UIImage? = nil, bannerImage: UIImage? = nil, username: String? = nil, completion completionHandler: @escaping (_ result: Result<UserUpdate, KKAPIError>) -> Void) {
+	///
+	/// - Returns: An instance of `RequestSender` with the results of the update information response.
+	public func updateInformation(_ profileUpdateRequest: ProfileUpdateRequest) -> RequestSender<UserUpdateResponse, KKAPIError> {
+		// Prepare headers
+		var headers: HTTPHeaders = [
+			.contentType("multipart/form-data"),
+			.accept("application/json"),
+			.authorization(bearerToken: self.authenticationKey)
+		]
+		var parameters: [String: Any] = [:]
+
+		if let username = profileUpdateRequest.username {
+			parameters["username"] = username
+		}
+		if let username = profileUpdateRequest.nickname {
+			parameters["nickname"] = username
+		}
+		if let biography = profileUpdateRequest.biography {
+			parameters["biography"] = biography
+		}
+		if profileUpdateRequest.profileImage == nil {
+			parameters["profileImage"] = "null"
+		}
+		if profileUpdateRequest.bannerImage == nil {
+			parameters["bannerImage"] = "null"
+		}
+
+		// Prepare request
 		let usersProfile = KKEndpoint.Me.update.endpointValue
 		let request: UploadAPIRequest<UserUpdateResponse, KKAPIError> = tron.codable.uploadMultipart(usersProfile) { formData in
-			if let profileImage = profileImage {
-				if !profileImage.isEqual(UIImage()) {
-					if let profileImage = profileImage.jpegData(compressionQuality: 0.1) {
-						formData.append(profileImage, withName: "profileImage", fileName: "ProfileImage.jpg", mimeType: "image/jpg")
-					}
+			if let profileImageURL = profileUpdateRequest.profileImage, let profileImageData = try? Data(contentsOf: profileImageURL) {
+				let pathExtension = profileImageURL.pathExtension
+				var uploadImage: Data!
+
+				switch pathExtension {
+				case "jpeg", "jpg", "png":
+					uploadImage = UIImage(data: profileImageData)?.jpegData(compressionQuality: 0.1) ?? profileImageData
+				default:
+					uploadImage = profileImageData
 				}
+
+				formData.append(uploadImage, withName: "profileImage", fileName: profileImageURL.lastPathComponent, mimeType: "image/\(pathExtension)")
 			}
-			if let bannerImage = bannerImage {
-				if !bannerImage.isEqual(UIImage()) {
-					if let bannerImageData = bannerImage.jpegData(compressionQuality: 0.1) {
-						formData.append(bannerImageData, withName: "bannerImage", fileName: "BannerImage.jpg", mimeType: "image/jpg")
-					}
+			if let bannerImageURL = profileUpdateRequest.bannerImage, let bannerImageData = try? Data(contentsOf: bannerImageURL) {
+				let pathExtension = bannerImageURL.pathExtension
+				var uploadImage: Data!
+
+				switch pathExtension {
+				case "jpeg", "jpg", "png":
+					uploadImage = UIImage(data: bannerImageData)?.jpegData(compressionQuality: 0.1) ?? bannerImageData
+				default:
+					uploadImage = bannerImageData
 				}
+
+				formData.append(uploadImage, withName: "bannerImage", fileName: bannerImageURL.lastPathComponent, mimeType: "image/\(pathExtension)")
 			}
 		}
+			.method(.post)
+			.parameters(parameters)
+			.headers(headers)
 
-		request.headers = [
-			.contentType("multipart/form-data"),
-			.accept("application/json")
-		]
-		request.headers.add(.authorization(bearerToken: self.authenticationKey))
-
-		request.method = .post
-		if let biography = biography {
-			request.parameters["biography"] = biography
-		}
-		if let username = username {
-			request.parameters["username"] = username
-		}
-		if profileImage?.isEqual(UIImage()) ?? false {
-			request.parameters["profileImage"] = "null"
-		}
-		if bannerImage?.isEqual(UIImage()) ?? false {
-			request.parameters["bannerImage"] = "null"
-		}
-
-		request.perform(withSuccess: { userUpdateResponse in
-			User.current?.attributes.update(using: userUpdateResponse.data)
-			completionHandler(.success(userUpdateResponse.data))
-		}, failure: { [weak self] error in
-			guard let self = self else { return }
-			UIView().endEditing(true)
-			if self.services.showAlerts {
-				UIApplication.topViewController?.presentAlertController(title: "Can't Update Information 😔", message: error.message)
-			}
-			print("❌ Received update profile information error", error.errorDescription ?? "Unknown error")
-			print("┌ Server message:", error.message ?? "No message")
-			print("├ Recovery suggestion:", error.recoverySuggestion ?? "No suggestion available")
-			print("└ Failure reason:", error.failureReason ?? "No reason available")
-			completionHandler(.failure(error))
-		})
+		// Send request
+		return request.sender()
 	}
 
 	/// Fetch the followers or following list for the authenticated user.
