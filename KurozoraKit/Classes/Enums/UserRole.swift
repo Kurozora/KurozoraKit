129806e625adeee3649356b6f0124085ca2a8554
kiritokatklian <casillaskhoren1@gmail.com>
2024-03-26 18:28:20 +0100

    [Update] User Attributes - Added UserRole enum - Updated UserAttributes.role to use the new UserRole enum

--- KurozoraKit/Classes/Enums/UserRole.swift
+++ KurozoraKit/Classes/Enums/UserRole.swift
@@ -6,3 +6,20 @@
 //
 
 import Foundation
+
+/// The set of available user role types.
+///
+/// ```
+/// case superAdmin = 1
+/// case admin = 2
+/// case editor = 3
+/// ```
+public enum UserRole: Int, Codable {
+	// MARK: - Cases
+	/// Indicates the user has the super admin role.
+	case superAdmin = 1
+	/// Indicates the user has the admin role.
+	case admin = 2
+	/// Indicates the user has the editor role.
+	case editor = 3
+}
