791634815029408d86cbb0e76964c6a852edfc73
kiritokatklian <casillaskhoren1@gmail.com>
2025-11-22 23:38:21 +0100

    feat: update `KurozoraRequestable` protocol with associated type and data property - Added associated type `Item` conforming to `KurozoraItem` - Added `data` property to hold an array of `Item`

--- KurozoraKit/Classes/Protocols/KurozoraRequestable.swift
+++ KurozoraKit/Classes/Protocols/KurozoraRequestable.swift
@@ -8,4 +8,10 @@
 import Foundation
 
 /// A protocol for Kurozora items that your app can fetch by using a library request.
-public protocol KurozoraRequestable: Decodable {}
+public protocol KurozoraRequestable: Decodable {
+	/// The type of Kurozora item included in the response.
+	associatedtype Item: KurozoraItem
+
+	/// The data included in the response for a requestable object request.
+	var data: [Item] { get }
+}
